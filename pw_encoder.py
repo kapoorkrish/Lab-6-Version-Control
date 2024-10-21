24377ccb30a6cd5e904c6514f0596c99969fc84a
sh1wnnn <shawnickwang@gmail.com>
2024-10-21 17:00:12 -0400

    Added and implemented decode() function

--- pw_encoder.py
+++ pw_encoder.py
@@ -11,6 +11,15 @@ def encode(pw: str):
 
     return encoded
 
+def decode(password):
+    decoded_password = ""
+    for num in password:
+        if (int(num) - 3 < 0):
+            decoded_password += str((int(num) - 3) + 10)
+        else:
+            decoded_password += str(int(num) - 3)
+    return decoded_password
+
 if __name__ == "__main__":
     pw = ""
 
@@ -31,6 +40,6 @@ if __name__ == "__main__":
 
             print("Your password has been encoded and stored!\n")
         elif option == 2:
-            print(f"The encoded password is {pw}, and the original password is .\n")
+            print(f"The encoded password is {pw}, and the original password is {decode(pw)}.\n")
         elif option == 3:
             break
